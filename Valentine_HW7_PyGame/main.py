0edbb1970e337bcc55705e5f339007d8ab1f5aaf
charisval <charischerryberry@gmail.com>
2025-03-05 19:54:37 +0000

    HW #8 Moving Objects

--- Valentine_HW7_PyGame/main.py
+++ Valentine_HW7_PyGame/main.py
@@ -1,11 +1,12 @@
 """
 Name: Charis Valentine
-Date: 3/3/25
-Homework: PyGame
-Description:
-This is a program that draws shapes on the screen, including three lanes, a frog, and three different types of vehicles.
+Date: 3/10/25
+Homework: HW8 Moving Objects
+Description: Program where frog can be moved across the screen using WASD and three cars move across the screen.
+
 """
 import pygame
+from pygame.constants import KEYDOWN
 
 # initialize Pygame
 pygame.init()
@@ -25,12 +26,50 @@ speed = 10
 """game loop"""
 running = True
 
+# frog current position
+frog_pos = [275, 340]
+
+# vehicles current positions
+truck_pos = [0, 76]
+pcar_pos = [525, 176]
+ycar_pos = [0, 275]
+
 while running:
   """handle events"""
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+    if event.type == KEYDOWN:
+      if event.key == pygame.K_w:
+        frog_pos[1] -= 20
+      if event.key == pygame.K_s:
+        frog_pos[1] += 20
+      if event.key == pygame.K_a:
+        frog_pos[0] -= 20
+      if event.key == pygame.K_d:
+        frog_pos[0] += 20
   """update game state"""
+  # x axis bounds for frog
+  if frog_pos[0] < 0:
+    frog_pos[0] = 0
+  if frog_pos[0] > width - 40:
+    frog_pos[0] = width - 40
+  # y axis bounds for frog
+  if frog_pos[1] < 0:
+    frog_pos[1] = 0
+  if frog_pos[1] > height - 40:
+    frog_pos[1] = height - 40
+  # car movements
+  truck_pos[0] += 20
+  ycar_pos[0] += 15
+  pcar_pos[0] -= 20
+  # car loops - cars will respawn after reaching the edge
+  if truck_pos[0] == width:
+    truck_pos[0] = 0
+  if ycar_pos[0] == width:
+    ycar_pos[0] = 0
+  if pcar_pos[0] < 0:
+    pcar_pos[0] = 525
   """draw on screen"""
 
   # clear screen
@@ -48,16 +87,20 @@ while running:
   pygame.draw.rect(screen, (255, 212, 229), pygame.Rect((0, 0), (600, 71)))
 
   # draw frog
-  pygame.draw.rect(screen, (128, 239, 128), pygame.Rect((275, 340), (40, 40)))
+  pygame.draw.rect(screen, (128, 239, 128),
+                   pygame.Rect((frog_pos[0], frog_pos[1]), (40, 40)))
 
   # purple truck
-  pygame.draw.rect(screen, (219, 220, 255), pygame.Rect((0, 76), (120, 50)))
+  pygame.draw.rect(screen, (219, 220, 255),
+                   pygame.Rect((truck_pos[0], truck_pos[1]), (120, 50)))
 
   # pink car
-  pygame.draw.rect(screen, (238, 203, 255), pygame.Rect((0, 176), (75, 50)))
+  pygame.draw.rect(screen, (238, 203, 255),
+                   pygame.Rect((pcar_pos[0], pcar_pos[1]), (75, 50)))
 
   # yellow car
-  pygame.draw.rect(screen, (254, 255, 163), pygame.Rect((0, 275), (75, 50)))
+  pygame.draw.rect(screen, (254, 255, 163),
+                   pygame.Rect((ycar_pos[0], ycar_pos[1]), (75, 50)))
 
   # update screen
   pygame.display.flip()
